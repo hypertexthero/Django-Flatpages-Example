61d63d68315b5c3d194dc1c11dbb6d327d8097b8
Simon Griffee <simon@hypertexthero.com>
2011-04-05 13:51:19 +0200

    added static files and a stylesheet to test if it is working

--- settings-example.py
+++ settings-example.py
@@ -45,12 +45,12 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = '/full/path/to/mysite/media/'
+MEDIA_ROOT = '/full/path/to/mysite/myproject/media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = 'http://127.0.0.1:8000/media/'
+MEDIA_URL = ''
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -72,6 +72,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+	'/full/path/to/mysite/myproject/static'
 )
 
 # List of finder classes that know how to find static files in
--- urls.py
+++ urls.py
@@ -1,5 +1,9 @@
 from django.conf.urls.defaults import patterns, include, url
 
+# Importing staticfiles_urlpatterns 
+# - http://docs.djangoproject.com/en/1.3/howto/static-files/#staticfiles-development
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
@@ -18,3 +22,6 @@ urlpatterns += patterns('django.views.generic.simple',
     (r'^$',             'direct_to_template', {'template': 'base.html'}),
     # (r'^foo/(?P<id>\d+)/$', 'direct_to_template', {'template': 'foo_detail.html'}),
 )
+
+# Importing staticfiles_urlpatterns
+urlpatterns += staticfiles_urlpatterns()
